f35cf35f7fe1a7b69a101ac1941a9c9c4a09398a
Mike Patrick <45206934+michaelspatrick@users.noreply.github.com>
2022-09-12 12:58:06 -0400

    Update whitelist-firewall.sh

--- whitelist-firewall.sh
+++ whitelist-firewall.sh
@@ -2,8 +2,14 @@
 #
 # Cron entry: @reboot /bin/sleep 60 ; /bin/bash -c ". /home/centos/.bashrc ; /home/centos/whitelist-firewall.sh"
 #
-# Based upon: https://bobcares.com/blog/iptables-whitelist-ip/
-# Reads a file of IP addresses to whitelist
+# Based upon: https://linux-audit.com/blocking-ip-addresses-in-linux-with-iptables/
+# Reads a file of IP addresses and adds them to the whitelist
+#
+# Initial Setup
+# Create whitelist with ipset utility (once)
+# sudo ipset create whitelist hash:ip hashsize 4096
+# sudo iptables -I INPUT -m set --match-set whitelist src -j ACCEPT
+# sudo iptables -I FORWARD -m set --match-set whitelist src -j ACCEPT
 #
 
 IPFILE="/home/centos/IPs-whitelist.txt"
@@ -11,5 +17,5 @@ IPFILE="/home/centos/IPs-whitelist.txt"
 for IP in $(cat $IPFILE)
 do
   echo "Whitelisting $IP"
-  iptables -A INPUT -s $IP -j ACCEPT
+  ipset add whitelist $IP
 done
